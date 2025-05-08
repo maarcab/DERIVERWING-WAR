6b0747904cd558565ad2a599b12686c322857e02
maarcab <maragonesb@edu.tecnocampus.cat>
2025-05-08 20:17:47 +0200

    Botó
    Script i test del botó complerts. El botó fa que una plataforma vagi de un punt A a un punt B.

--- DeriverWing War/Assets/MyAssets/Scripts/PlatformMover.cs
+++ DeriverWing War/Assets/MyAssets/Scripts/PlatformMover.cs
@@ -4,33 +4,51 @@ using UnityEngine;
 public class PlatformMover : MonoBehaviour
 {
     private Vector3 currentTarget;
+    private bool isMoving = false;
 
     [SerializeField] private float speed = 2f;
     [SerializeField] private Transform start, end;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentTarget = start.position;
+        currentTarget = end.position;
     }
-     
-    // Update is called once per frame
+
     void Update()
     {
-        if (transform.position == start.position)
-        {
-            currentTarget = end.position;
-        }
+        if (!isMoving) return;
 
-        if (transform.position == end.position)
-        {
-            currentTarget = start.position;
-        }
         Move();
 
+        if (transform.position.x >= currentTarget.x - 0.1f && transform.position.x <= currentTarget.x + 0.1f && transform.position.y >= currentTarget.y - 0.1f && transform.position.y <= currentTarget.y + 0.1f)
+        {
+            isMoving = false;
+            ToggleTarget();
+        }
     }
+
     void Move()
     {
         transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
+
+    public void StartMoving()
+    {
+        if (isMoving) return;
+
+        isMoving = true;
+    }
+
+    private void ToggleTarget()
+    {
+        if (transform.position.x >= start.position.x - 0.5f && transform.position.x <= start.position.x + 0.5f && transform.position.y >= start.position.y - 0.5f && transform.position.y <= start.position.y + 0.5f)
+        {
+            Debug.Log("Arribaaa");
+            currentTarget = end.position;
+        }
+        else if (transform.position.x >= end.position.x - 0.5f && transform.position.x <= end.position.x + 0.5f && transform.position.y >= end.position.y - 0.5f && transform.position.y <= end.position.y + 0.5f)
+        {
+            currentTarget = start.position;
+        }
     }
 }
