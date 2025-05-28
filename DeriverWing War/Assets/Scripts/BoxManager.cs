d12651843e0ea2113c7922e6472bc8fae9ed39a3
maarcab <maragonesb@edu.tecnocampus.cat>
2025-05-28 15:09:29 +0200

    First approach
    Barra de vida de la caixa, fongs (arreglar sprite)

--- DeriverWing War/Assets/Scripts/BoxManager.cs
+++ DeriverWing War/Assets/Scripts/BoxManager.cs
@@ -4,8 +4,13 @@ using UnityEngine.UI;
 
 public class BoxManager : MonoBehaviour
 {
-    [SerializeField] private Transform boxSpawner;
+    [SerializeField] private Transform BoxSpawner;
     public Text WINTEXT;
+    public Image durabilityBar;
+    public float durability, maxDurability;
+    public float damage;
+    private bool isDecomposing = false;
+    private float timer;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -19,12 +24,53 @@ public class BoxManager : MonoBehaviour
             WINTEXT.gameObject.SetActive(true);
             Time.timeScale = 0;
         }
+
+        if (collision.CompareTag("Hongo"))
+        {
+            isDecomposing = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hongo"))
+        {
+            isDecomposing = false;
+            timer = 0f;
+        }
+    }
+    private void Update()
+    {
+        if (isDecomposing)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 1f)
+            {
+                Decompose();
+                timer = 0f;
+            }
+        }
+        if (Input.GetKeyDown("f"))
+        {
+            Decompose();
+        }
     }
 
     void Respawn()
     {
-        this.transform.position = boxSpawner.position;
-        this.transform.rotation = boxSpawner.rotation;
+        this.transform.position = BoxSpawner.position;
+        this.transform.rotation = BoxSpawner.rotation;
+    }
+    void Decompose()
+    {
+        durability -= damage;
+        durabilityBar.fillAmount = durability / maxDurability;
+        if (durability <= 0)
+        {
+            timer = 0f;
+            durability = maxDurability;
+            durabilityBar.fillAmount = 1f;
+            Respawn();
+        }
     }
 }
 
